6829a6b9ce76a8c46c96d704b91ca522d185453e
Tony <tonylovepony@gmail.com>
2020-03-11 01:23:17 +0200

    upd

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
 include "vendor/autoload.php";
 
-$requester = new \Xtodx\Rozetka\RozetkaRequest("AgrusRozetka", "5zt00g9xdwrl", __DIR__ . "/rozetka.txt", __DIR__ . "/cookie.txt");
+$requester = new \Xtodx\Rozetka\RozetkaRequest("rozetka login", "rozetka password", __DIR__ . "/rozetka.txt", __DIR__ . "/cookie.txt");
 
 //$login = $requester->login();
 $orders = $requester->request('orders/search');
